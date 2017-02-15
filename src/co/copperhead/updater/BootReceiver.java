620961fd3928ce8379339f28c222923f075d7b22
Daniel Micay <danielmicay@gmail.com>
2017-02-15 15:55:10 -0500

    move job scheduling code to PeriodicJob

--- src/co/copperhead/updater/BootReceiver.java
+++ src/co/copperhead/updater/BootReceiver.java
@@ -1,9 +1,6 @@
 package co.copperhead.updater;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -12,36 +9,14 @@ import co.copperhead.updater.PeriodicJob;
 
 public class BootReceiver extends BroadcastReceiver {
     private static final String TAG = "BootReceiver";
-    private static final int JOB_ID = 1;
-    private static final long INTERVAL_MILLIS = 60 * 60 * 1000;
-    private static final int NETWORK_TYPE = JobInfo.NETWORK_TYPE_ANY;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
 
         if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-            JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-            JobInfo jobInfo = scheduler.getPendingJob(JOB_ID);
-            if (jobInfo != null &&
-                    jobInfo.getNetworkType() == NETWORK_TYPE &&
-                    jobInfo.isPersisted() &&
-                    jobInfo.getIntervalMillis() == INTERVAL_MILLIS) {
-                Log.d(TAG, "Job already registered");
-                return;
-            }
-            ComponentName serviceName = new ComponentName(context, PeriodicJob.class);
-            jobInfo = new JobInfo.Builder(JOB_ID, serviceName)
-                .setRequiredNetworkType(NETWORK_TYPE)
-                .setPersisted(true)
-                .setPeriodic(INTERVAL_MILLIS)
-                .build();
-            int result = scheduler.schedule(jobInfo);
-            if (result == JobScheduler.RESULT_SUCCESS) {
-                Log.d(TAG, "Job schedule success");
-            } else {
-                Log.d(TAG, "Job schedule failed");
-            }
+            Log.d(TAG, "schedule PeriodicJob");
+            PeriodicJob.schedule(context);
         } else {
             Log.d(TAG, "unhandled action: " + action);
         }
--- src/co/copperhead/updater/PeriodicJob.java
+++ src/co/copperhead/updater/PeriodicJob.java
@@ -1,12 +1,43 @@
 package co.copperhead.updater;
 
-import android.app.job.JobService;
+import android.app.job.JobInfo;
 import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 public class PeriodicJob extends JobService {
     private static final String TAG = "PeriodicJob";
+    private static final int JOB_ID = 1;
+    private static final int NETWORK_TYPE = JobInfo.NETWORK_TYPE_ANY;
+    private static final long INTERVAL_MILLIS = 60 * 60 * 1000;
+
+    static void schedule(Context context) {
+        JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo jobInfo = scheduler.getPendingJob(JOB_ID);
+        if (jobInfo != null &&
+                jobInfo.getNetworkType() == NETWORK_TYPE &&
+                jobInfo.isPersisted() &&
+                jobInfo.getIntervalMillis() == INTERVAL_MILLIS) {
+            Log.d(TAG, "Job already registered");
+            return;
+        }
+        ComponentName serviceName = new ComponentName(context, PeriodicJob.class);
+        jobInfo = new JobInfo.Builder(JOB_ID, serviceName)
+            .setRequiredNetworkType(NETWORK_TYPE)
+            .setPersisted(true)
+            .setPeriodic(INTERVAL_MILLIS)
+            .build();
+        int result = scheduler.schedule(jobInfo);
+        if (result == JobScheduler.RESULT_SUCCESS) {
+            Log.d(TAG, "Job schedule success");
+        } else {
+            Log.d(TAG, "Job schedule failed");
+        }
+    }
 
     @Override
     public boolean onStartJob(JobParameters params) {
