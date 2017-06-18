326dedd209f922a89b29cf8fcf2f2415647f28d6
Daniel Micay <danielmicay@gmail.com>
2017-06-18 00:10:06 -0400

    only run for the system user

--- src/co/copperhead/updater/BootReceiver.java
+++ src/co/copperhead/updater/BootReceiver.java
@@ -3,10 +3,15 @@ package co.copperhead.updater;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserManager;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        PeriodicJob.schedule(context);
+        if (UserManager.get(context).isSystemUser()) {
+            PeriodicJob.schedule(context);
+        } else {
+            PeriodicJob.cancel(context);
+        }
     }
 }
--- src/co/copperhead/updater/Settings.java
+++ src/co/copperhead/updater/Settings.java
@@ -3,6 +3,7 @@ package co.copperhead.updater;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.UserManager;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -21,6 +22,9 @@ public class Settings extends PreferenceActivity {
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!UserManager.get(this).isSystemUser()) {
+            throw new SecurityException("system user only");
+        }
         getPreferenceManager().setStorageDeviceProtected();
         addPreferencesFromResource(R.xml.settings);
         final Preference networkType = findPreference(KEY_NETWORK_TYPE);
