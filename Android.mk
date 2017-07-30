f505bb104d16a4aa744f55870b690bfdb84fdb96
Daniel Micay <danielmicay@gmail.com>
2017-07-30 13:43:56 -0400

    device-independent battery optimization exception

--- Android.mk
+++ Android.mk
@@ -1,10 +1,18 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := co.copperhead.updater.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := Updater
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_REQUIRED_MODULES := co.copperhead.updater.xml
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
-
 include $(BUILD_PACKAGE)
