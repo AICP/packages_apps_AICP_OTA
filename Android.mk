e3f71ca1a4085edd2686d7de32c3c3783644361a
Daniel Micay <danielmicay@gmail.com>
2017-09-10 02:19:00 -0400

    add permissions whitelist file

--- Android.mk
+++ Android.mk
@@ -1,7 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := co.copperhead.updater.xml
+LOCAL_MODULE := permissions_co.copperhead.updater.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := whitelist_co.copperhead.updater.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
@@ -12,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := Updater
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_REQUIRED_MODULES := co.copperhead.updater.xml
+LOCAL_REQUIRED_MODULES := permissions_co.copperhead.updater.xml whitelist_co.copperhead.updater.xml
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 include $(BUILD_PACKAGE)
