c0268df88f389c88b4434771e6919bb2bf372ccc
Daniel Micay <danielmicay@gmail.com>
2017-09-20 17:20:03 -0400

    add default permissions configuration

--- Android.mk
+++ Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := default-permissions_co.copperhead.updater.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := permissions_co.copperhead.updater.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
@@ -20,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := Updater
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_REQUIRED_MODULES := permissions_co.copperhead.updater.xml whitelist_co.copperhead.updater.xml
+LOCAL_REQUIRED_MODULES := default-permissions_co.copperhead.updater.xml permissions_co.copperhead.updater.xml whitelist_co.copperhead.updater.xml
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 include $(BUILD_PACKAGE)
